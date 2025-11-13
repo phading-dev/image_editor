6e2186289cde217501cdd0a8ec6442d655a68f93
Ykuyo <ykuyo.dev@gmail.com>
2025-11-12 18:12:56 -0800

    Updaed SSL secrets files.

--- env_const.ts
+++ env_const.ts
@@ -2,5 +2,5 @@ import { ENV_VARS } from "./env_vars";
 
 ENV_VARS.vertexLocation = "us-central1";
 ENV_VARS.geminiModel = "gemini-2.5-flash";
-ENV_VARS.sslPrivateKeyFile = "phading.key";
-ENV_VARS.sslCertificateFile = "phading.crt";
+ENV_VARS.sslPrivateKeyFile = "private.key";
+ENV_VARS.sslCertificateFile = "cert.key";
--- env_prod.ts
+++ env_prod.ts
@@ -3,7 +3,7 @@ import { ENV_VARS } from "./env_vars";
 
 ENV_VARS.projectId = "image-editor-477100";
 ENV_VARS.enableHttps = true;
-ENV_VARS.gcsSecretBucketName = "phading-prod-image-editor";
+ENV_VARS.gcsSecretBucketName = "phading-image-editor-prod-secrets";
 ENV_VARS.externalDomain = "layra.app";
 ENV_VARS.externalOrigin = `https://${ENV_VARS.externalDomain}`;
 ENV_VARS.httpPort = 80;
